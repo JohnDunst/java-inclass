fffc184ee05880a0fab4c8d95ae2b026973f8dcf
JohnDunst <johnnydunst6@outlook.com>
2023-06-05 09:42:23 -0400

    resurant menu  java spaghetti

---
+++ array.js
@@ -0,0 +1,24 @@
+const breakfast= ["Bacon", "Eggs", "Sasuage", "pancakes", "toast", "biscuit"];
+const lunch = ["Hamburger", "Hotdog", "chicken sandwitch", "chicken tenders"]
+const dinner = ["Steak", "1/2 chicken", "hamburger", "hotdog", "Ribs", "Brisket"]
+const locations = ["Soutside", "SquirrelHill", "Oakland", "Cranberry"]
+const beverages = ["Ice Tea", "Coke", "Sprite" ]
+var happy_hour = 6;
+var happy_hour = false;
+    if (happy_hour >= 6) {
+        happy_hour = true;
+    }
+var bacon= 1.99
+var Eggs= 1.00
+var Sasuage= 1.25
+var pancakes= 2.00
+var toast=.50
+var biscuit=.75
+var hamburger= 4.50
+var hotdog= 1.75
+var steak= 10.99
+var ribs= 20.99
+var brisket= 15.99
+var Coke= .98
+var Sprite=.87
+
